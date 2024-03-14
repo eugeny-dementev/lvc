28f93cc9f4fe0f1ca47b717523fc8f2a3217b79d
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-03-14 13:51:27 +0800

    remove dependency on telescope for harpoon

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -57,7 +57,6 @@ return {
     branch = "harpoon2",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
     },
     config = function ()
       local harpoon = require('harpoon');
