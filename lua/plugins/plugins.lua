b76052801fb80a154faa6232424835bd11079a45
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-04-02 18:59:21 +0800

    add neotest-jest

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -116,6 +116,26 @@ return {
       })
     end,
   },
+
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "haydenmeade/neotest-jest",
+    },
+    opts = function (_, opts)
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "npm test --",
+          jestConfigFile = 'jest.config.js',
+          env = { CI = true },
+          cwd = function ()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
+    end,
+  },
 }
 
 
