f3b013108234b9c9939b28ea502b720c68cf34f8
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-29 22:16:30 +0800

    switch telescope to custom find files method for <leader><leader>

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -5,6 +5,31 @@ local js_languages = {
   "typescriptreact",
 }
 
+local my_find_files
+my_find_files = function(opts, no_ignore)
+  opts = opts or {}
+  no_ignore = vim.F.if_nil(no_ignore, false)
+  opts.attach_mappings = function(_, map)
+    map({ "n", "i" }, "<C-h>", function(prompt_bufnr) -- <C-h> to toggle modes
+      local prompt = require("telescope.actions.state").get_current_line()
+      require("telescope.actions").close(prompt_bufnr)
+      no_ignore = not no_ignore
+      my_find_files({ default_text = prompt }, no_ignore)
+    end)
+    return true
+  end
+
+  if no_ignore then
+    opts.no_ignore = true
+    opts.hidden = true
+    opts.prompt_title = "Find Files <ALL>"
+    require("telescope.builtin").find_files(opts)
+  else
+    opts.prompt_title = "Find Files"
+    require("telescope.builtin").find_files(opts)
+  end
+end
+
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
@@ -33,6 +58,12 @@ return {
     branch = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
+      {
+        "<leader><leader>",
+        my_find_files,
+        mode = "n",
+        desc = "Toggleable file search"
+      },
       {
         "<leader>pwf",
         function()
