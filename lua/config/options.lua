e24deb181ee161a773c7046c11b6ca2e1c8d68e3
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 16:08:43 +0800

    fix: apply patch to fix gopls ENOENT issue

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,9 +10,26 @@ vim.opt.spelloptions = "camel"
 if vim.fn.has("win32") == 1 and vim.fn.has("ws1") == 0 then
   vim.o.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
 
-  vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.o.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
   vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
   vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
   vim.o.shellquote = ""
   vim.o.shellxquote = ""
+
+  -- General/Global LSP Configuration
+  local api = vim.api
+  local lsp = vim.lsp
+
+  local make_client_capabilities = lsp.protocol.make_client_capabilities
+  function lsp.protocol.make_client_capabilities()
+    local caps = make_client_capabilities()
+    if not (caps.workspace or {}).didChangeWatchedFiles then
+      vim.notify("lsp capability didChangeWatchedFiles is already disabled", vim.log.levels.WARN)
+    else
+      caps.workspace.didChangeWatchedFiles = nil
+    end
+
+    return caps
+  end
 end
