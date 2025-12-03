022d1f00468d35895cb585d806ad7aac2726b7e1
Eugeny Dementev <eugeny.dementev@ya.ru>
2025-12-03 19:29:43 +0800

    fix: mini plugins repo rename

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
-  { "echasnovski/mini.comment", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-mini/mini.comment", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   -- { "rcarriga/nvim-notify", enabled = false },
